7787496db3e86643fb1db73b5ab9bd0f2973dc65
William <william.bjorklund.00@gmail.com>
2023-11-16 22:27:46 +0100

    Fixed contactsboxes

--- src/Components/BoxContacts.js
+++ src/Components/BoxContacts.js
@@ -1,37 +1,48 @@
-import React from 'react'
-import img_Icon11 from '../assets/images/Icon (4).png'
-import img_Icon22 from '../assets/images/Icon (5).png'
-import img_Icon33 from '../assets/images/Icon (6).png'
+import React from 'react';
+import img_Icon11 from '../assets/images/Icon (4).png';
+import img_Icon22 from '../assets/images/Icon (5).png';
+import img_Icon33 from '../assets/images/Icon (6).png';
 
 const BoxContacts = () => {
   return (
     <div className="container-boxes">
-  <div className="box visit">
-  <img className="Icon11" src={img_Icon11} alt=""/>
-    <div className="text">
-      <h4>Visit us</h4>
-      <p className="gray">Trädgårdsgatan 63</p>
-      <p className="gray">265 36 ÅSTORP</p>
-    </div>
-  </div>
-  <div className="box call">
-  <img className="Icon22" src={img_Icon22} alt=""/>
-    <div className="text">
-      <h4>Call us</h4>
-      <p className="gray">+46 (8) 121 470 50</p>
-      <p className="gray">+46 (8) 121 470 51</p>
-    </div>
-  </div>
-  <div className="box email">
-  <img className="Icon33" src={img_Icon33} alt=""/>
-    <div className="text">
-      <h4>Email us</h4>
-      <p className="gray">pingispagarna@gmail.com</p>
-      <p className="gray">info@pingispagarna.se</p>
-    </div>
-  </div>
-</div>
-  )
-}
+      {/* Box 1 */}
+      <div className="box visit">
+        <a href="https://astorp.se/uppleva-och-gora/arena-astorp.html" target="_blank">
+          <img className="Icon" src={img_Icon11} alt="Visit us" />
+        </a>
+        <div className="text">
+          <h4>Visit us</h4>
+          <p className="gray">Trädgårdsgatan 63</p>
+          <p className="gray">265 36 ÅSTORP</p>
+        </div>
+      </div>
 
-export default BoxContacts
+      {/* Box 2 */}
+      <div className="box call">
+        <a href="tel:+4612147050">
+          <img className="Icon" src={img_Icon22} alt="Call us" />
+        </a>
+        <div className="text">
+          <h4>Call us</h4>
+          <p className="gray">+46 (8) 121 470 50</p>
+          <p className="gray">+46 (8) 121 470 51</p>
+        </div>
+      </div>
+
+      {/* Box 3 */}
+      <div className="box email">
+        <a href="mailto:pingispagarna@gmail.com">
+          <img className="Icon" src={img_Icon33} alt="Email us" />
+        </a>
+        <div className="text">
+          <h4>Email us</h4>
+          <p className="gray">pingispagarna@gmail.com</p>
+          <p className="gray">info@pingispagarna.se</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoxContacts;
